78bd000582fe263a21271c886f705cd3431729c0
Andrew <andreivalitov2001@gmail.com>
2020-11-10 17:47:48 +0700

    Added google test to exception-throw commands

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
-#include "CalculatorApplication.h"
-#include "Exceptions/RuntimeCalculatorException.h"
-
-#include <iostream>
-
-int main(int argc, char *argv[]) {
-    Calculator::CalculatorApplication calculator;
-    auto dataEntry = static_cast<Calculator::DataEntryArguments>(argc);
-    try {
-        calculator.launch(dataEntry, argv[1]);
-    } catch (Calculator::RuntimeCalculatorException &exception) {
-        std::cout << exception.what() << std::endl;
-    }
-    return 0;
-}
+//#include "CalculatorApplication.h"
+//#include "Exceptions/RuntimeCalculatorException.h"
+//
+//#include <iostream>
+//
+//int main(int argc, char *argv[]) {
+//    Calculator::CalculatorApplication calculator;
+//    auto dataEntry = static_cast<Calculator::DataEntryArguments>(argc);
+//    try {
+//        calculator.launch(dataEntry, argv[1]);
+//    } catch (Calculator::RuntimeCalculatorException &exception) {
+//        std::cout << exception.what() << std::endl;
+//    }
+//    return 0;
+//}
